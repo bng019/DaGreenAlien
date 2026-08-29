90ca2bc843134041b7d019fbea7de9f24d34f3ad
steveng-1226 <steveng1226@gmail.com>
2026-08-28 23:12:43 -0700

    removed testing invo code

--- scripts/display_invo.gd
+++ scripts/display_invo.gd
@@ -1,9 +1,4 @@
 extends TextureButton #Button
 
 func _on_pressed(): #When this button gets clicked, displays everything in your inventory in Dialogic
-	Inventory.add_item("Bullet", 2)
-	Inventory.add_item("Gun")
-	Inventory.add_item("Nineteen dollar fortnite card", 1, 19)
-	Inventory.add_item("Shovel", 1, 50)
-	Inventory.add_item("six seven", 67, 67)
 	Dialogic.start("displayInvo")
